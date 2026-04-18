020d481da62b3c8e9ca0642b2289070db114961e
Tom Stophela <tstophela@users.noreply.github.com>
2026-04-18 17:31:41 +0800

    handle error returned from cmd.Execute and exit with non-zero status

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/your-org/vaultpull/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/your-org/vaultpull/cmd"
+)
 
 func main() {
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
